7a1020cb0dfc5eaf4b2dda30a3500f7190437027
Alex Murzinov <murzinov.sanek@gmail.com>
2023-08-31 15:42:00 +0400

    test_version

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QComboBox, QLineEdit, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QComboBox, QLineEdit, QPushButton, QVBoxLayout, QTextBrowser
 
 class CurrencyConverterApp(QMainWindow):
     def __init__(self):
@@ -34,6 +34,10 @@ class CurrencyConverterApp(QMainWindow):
         self.load_currencies()
         self.update_currency_combos()
 
+        self.currencies_text_browser = QTextBrowser()
+        layout.addWidget(self.currencies_text_browser)
+        self.update_currencies_list()
+
     def load_currencies(self):
         response = requests.get("https://www.cbr-xml-daily.ru/latest.js")
         data = response.json()
@@ -44,6 +48,13 @@ class CurrencyConverterApp(QMainWindow):
     def update_currency_combos(self):
         self.currency_combo_from.addItems(self.rates.keys())
         self.currency_combo_to.addItems(self.rates.keys())
+        self.currency_combo_from.setCurrentText("RUB")
+        self.currency_combo_to.setCurrentText("USD")
+
+    def update_currencies_list(self):
+        rub_rates = {currency: 1 / rate for currency, rate in self.rates.items()}
+        currencies_list = "\n".join([f"{currency}: {rate:.4f}" for currency, rate in rub_rates.items()])
+        self.currencies_text_browser.setPlainText(currencies_list)
 
     def convert(self):
         currency_from = self.currency_combo_from.currentText()
