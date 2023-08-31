aab349ce45719465d90e9e7817904bb82f7d6902
Alex Murzinov <murzinov.sanek@gmail.com>
2023-08-31 16:04:26 +0400

    final

--- main.py
+++ main.py
@@ -1,42 +1,54 @@
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QComboBox, QLineEdit, QPushButton, QVBoxLayout, QTextBrowser
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QComboBox, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QTextBrowser, QTableWidget, QTableWidgetItem
 
 class CurrencyConverterApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Currency Converter")
+        self.setGeometry(100, 100, 800, 400) 
 
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
 
-        layout = QVBoxLayout()
+        main_layout = QHBoxLayout()
+        self.central_widget.setLayout(main_layout)
+
+        left_panel = QWidget(self)
+        right_panel = QWidget(self)
+
+        main_layout.addWidget(left_panel)
+        main_layout.addWidget(right_panel)
+
+        left_layout = QVBoxLayout(left_panel)
+        self.currency_table = QTableWidget()
+        self.currency_table.setColumnCount(2)
+        self.currency_table.setHorizontalHeaderLabels(["Currency", "Exchange Rate"])
+        left_layout.addWidget(self.currency_table)
+
+        self.load_currencies()
+        self.update_currency_table()
+
+        right_layout = QVBoxLayout(right_panel)
 
         self.currency_combo_from = QComboBox()
         self.currency_combo_to = QComboBox()
 
-        layout.addWidget(self.currency_combo_from)
-        layout.addWidget(self.currency_combo_to)
+        right_layout.addWidget(self.currency_combo_from)
+        right_layout.addWidget(self.currency_combo_to)
 
         self.amount_line_edit = QLineEdit()
-        layout.addWidget(self.amount_line_edit)
-
-        self.result_label = QLabel()
-        layout.addWidget(self.result_label)
+        right_layout.addWidget(self.amount_line_edit)
 
         self.convert_button = QPushButton("Convert")
         self.convert_button.clicked.connect(self.convert)
-        layout.addWidget(self.convert_button)
+        right_layout.addWidget(self.convert_button)
 
-        self.central_widget.setLayout(layout)
+        self.result_label = QLabel()
+        right_layout.addWidget(self.result_label)
 
-        self.load_currencies()
         self.update_currency_combos()
-
-        self.currencies_text_browser = QTextBrowser()
-        layout.addWidget(self.currencies_text_browser)
-        self.update_currencies_list()
 
     def load_currencies(self):
         response = requests.get("https://www.cbr-xml-daily.ru/latest.js")
@@ -45,16 +57,19 @@ class CurrencyConverterApp(QMainWindow):
         self.rates = data["rates"]
         self.rates["RUB"] = 1.0  # Добавляем RUB с курсом 1.0
 
+    def update_currency_table(self):
+        self.currency_table.setRowCount(0)
+        for currency, rate in self.rates.items():
+            row_position = self.currency_table.rowCount()
+            self.currency_table.insertRow(row_position)
+            self.currency_table.setItem(row_position, 0, QTableWidgetItem(currency))
+            self.currency_table.setItem(row_position, 1, QTableWidgetItem(str(rate)))
+
     def update_currency_combos(self):
         self.currency_combo_from.addItems(self.rates.keys())
         self.currency_combo_to.addItems(self.rates.keys())
         self.currency_combo_from.setCurrentText("RUB")
         self.currency_combo_to.setCurrentText("USD")
-
-    def update_currencies_list(self):
-        rub_rates = {currency: 1 / rate for currency, rate in self.rates.items()}
-        currencies_list = "\n".join([f"{currency}: {rate:.4f}" for currency, rate in rub_rates.items()])
-        self.currencies_text_browser.setPlainText(currencies_list)
 
     def convert(self):
         currency_from = self.currency_combo_from.currentText()
